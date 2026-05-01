6c8965756c3c3783bc55e80d67e6f2609b8cf98e
IFCSapp <kenji.kitamura.if@gmail.com>
2026-05-01 16:59:50 +0900

    0.5.5

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "./",
+  // base: "./", 
+  // 開発環境では相対パスを使用、本番環境ではルートパスを使用
+  base: "/support-planning/",
   plugins: [react],
 });
